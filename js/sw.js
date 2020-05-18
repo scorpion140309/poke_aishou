60c80ac91e2cad0783ff38a7e8f46742d59f84ea
scorpion140309 <scorpion140309@yahoo.co.jp>
2020-05-18 17:33:13 +0900

    キャッシュの相対パスを変更してみた。

--- js/sw.js
+++ js/sw.js
@@ -1,14 +1,14 @@
 // キャッシュ名とキャッシュファイルの指定
 var CACHE_NAME = 'pwa-poke-aishou';
 var urlsToCache = [
-	"/../index.html",
-	"/../poke_aishou_h.html",
-	"/../poke_aishou_v.html",
-	"/../main.css",
-	"/../main_h.css",
-	"/../main_v.css",
-	"/poke_type_database.js",
-	"/poke_aishou.js"
+	"./index.html",
+	"./poke_aishou_h.html",
+	"./poke_aishou_v.html",
+	"./main.css",
+	"./main_h.css",
+	"./main_v.css",
+	"./js/poke_type_database.js",
+	"./js/poke_aishou.js"
 ];
 
 // インストール処理
