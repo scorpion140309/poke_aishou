6cf8dd4e3baa31af817e4594d5e1375d064685eb
scorpion140309 <scorpion140309@yahoo.co.jp>
2020-05-18 17:24:21 +0900

    init

---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# ポケモンGO相性
+
+このソフトはポケモンGOのタイプ別ダメージ率を表示します。
+
+![Screen Shot](poke_aishou.png "Screen Shot")
---
+++ js/sw.js
@@ -0,0 +1,34 @@
+// キャッシュ名とキャッシュファイルの指定
+var CACHE_NAME = 'pwa-poke-aishou';
+var urlsToCache = [
+	"/../index.html",
+	"/../poke_aishou_h.html",
+	"/../poke_aishou_v.html",
+	"/../main.css",
+	"/../main_h.css",
+	"/../main_v.css",
+	"/poke_type_database.js",
+	"/poke_aishou.js"
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
